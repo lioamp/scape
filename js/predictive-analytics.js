8d1c55298c6ea24a32eac35e772b75fca349fa42
lioamp <lioamp21@gmail.com>
2025-05-21 20:43:36 +0800

    fixed predictive analytics link to its own page, also fixed connection to the firebase js of it's js file

--- js/predictive-analytics.js
+++ js/predictive-analytics.js
@@ -2,17 +2,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 
-// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCyIr7hWhROGodkcsMJC9n4sEuDOR5NGww",
+  authDomain: "scape-login.firebaseapp.com",
+  projectId: "scape-login",
+  storageBucket: "scape-login.firebasestorage.app",
+  messagingSenderId: "410040228789",
+  appId: "1:410040228789:web:5b9b4b32e91c5549ab17fc",
+  measurementId: "G-GBNRL156FJ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
